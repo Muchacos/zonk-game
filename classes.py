786a259bd7177eb7206739ad17732d24b206b4b8
muchacos <muchacos@ya.ru>
2019-07-07 17:33:35 +0300

    v0.0.2 - 'Silly Amoeba'
    - The functionality of the dices is improved (rewritten) to the primitive
    functionality of the Zonk

--- classes.py
+++ classes.py
@@ -5,36 +5,154 @@ import random
 
 class Player:
 
-    def __init__(self, id, name, points=0):
-        self.id = id
+    def __init__(self, name):
         self.name = name
-        self.points = points
+        self.score = 0
+        self.score_turn = 0
 
-    def sayhi(self):
-        print('hi, my name is', self.name)
+    def add_scoretotal(self):
+        self.score += self.score_turn
+        self.score_turn = 0
+
+    def add_scoreturn(self, score):
+        self.score_turn += score
+
+    def remove_scoreturn(self):
+        self.score_turn = 0
 
 
 class Game:
     game_flag = True
+    dices = [1]*6
 
-    def __init__(self, high_bar, curr_player, pend_player):
+    def __init__(self):
+        pass
+
+    def set_settings(self, player, high_bar, printer):
+        self.player = player
         self.high_bar = high_bar
-        self.curr_player = curr_player
-        self.pend_player = pend_player
+        self.printer = printer
 
     def win_check(self):
-        if self.curr_player.points >= self.high_bar:
+        if self.player.score >= self.high_bar:
             Game.game_flag = False
-            print('++++++++++++++++++++++++++++++')
-            print(self.curr_player.name, 'has won the game!')
-            print(self.pend_player.name, ', you suck!')
+            self.printer._print_won()
 
     def turn(self):
+        printer = self.printer
+
         if Game.game_flag:
-            d1, d2 = random.randint(1, 6), random.randint(1, 6)
-            self.curr_player.points += d1 + d2
-            print('Dices: %d %d | Total points: %d' % (d1, d2, self.curr_player.points))
-            self.win_check()
-            self.curr_player, self.pend_player = self.pend_player, self.curr_player
+            for i in range(len(Game.dices)):
+                Game.dices[i] = random.randint(1, 6)
+
+            printer._print_dices(Game.dices)
+
+            if 5 not in Game.dices and 1 not in Game.dices:
+                printer._print_nodices()
+                self.player.remove_scoreturn()
+                return False
+
+            turn_flag = printer.get_turncontinue()
+            if not turn_flag:
+                printer._print_scoreturn()
+                self.player.add_scoretotal()
+                printer._print_scoretotal()
+                self.win_check()
+                return False
+
+            pick = 0
+
+            while True:
+                pick = printer.get_dicechoose(Game.dices)
+
+                if Game.dices[pick] == 1:
+                    printer._print_scoreearned(100)
+                    self.player.add_scoreturn(100)
+                    break
+                elif Game.dices[pick] == 5:
+                    printer._print_scoreearned(50)
+                    self.player.add_scoreturn(50)
+                    break
+                else:
+                    printer._print_wrongdice()
+            Game.dices.pop(pick)
+            printer._print_scoreturn()
+            return True
+
         else:
-            print('The game is ended')
+            printer._print_gameend()
+            return False
+
+    def add_dices(self):
+        Game.dices.extend([1]*(6 - len(Game.dices)))
+
+
+class Printer:
+
+    def __init__(self, game_mode):
+        self.game_mode = game_mode
+
+    def get_settings(self):
+        print('Welcome to the game! What is your name?')
+        p = Player(input())
+        print('Ok... let me write it down... yep.\nNow, choose a maximum fo points.')
+        high_bar = int(input())
+        print('Great, we can start!')
+        return (p, high_bar)
+
+    def get_dicechoose(self, dices):
+        print('\nPick a dice!')
+        pick = 0
+        while True:
+            try:
+                val = int(input('dice: '))
+                pick = dices.index(val)
+            except:
+                continue
+            else:
+                break
+        return pick
+
+    def get_turncontinue(self):
+        ans = ''
+        print('We continue?')
+
+        while ans not in ('y', 'n'):
+            ans = input('y/n: ')
+        if ans == 'y':
+            return True
+        elif ans == 'n':
+            return False
+
+    def _print_turnstart(self):
+        gm = self.game_mode
+        print('------------------------------')
+        print(gm.player.name, ', your Turn!')
+
+    def _print_dices(self, dices):
+        str = ''
+        for i in range(len(dices)):
+            str += '[%d] ' % dices[i]
+        print(str)
+
+    def _print_scoreturn(self):
+        print('Turn score:', self.game_mode.player.score_turn)
+
+    def _print_scoretotal(self):
+        score = self.game_mode.player.score
+        print('Total score:', score)
+
+    def _print_scoreearned(self, score):
+        print('You earned %d points!' % score)
+
+    def _print_nodices(self):
+        print('No dices to pick!')
+
+    def _print_wrongdice(self):
+        print('You can\'t pick this dice')
+
+    def _print_won(self):
+        print('CONGRATS! YOU WON!')
+
+    def _print_gameend(self):
+        print('Game alrady end')
--- game.py
+++ game.py
@@ -1,20 +1,13 @@
-from classes import Player, Game
+from classes import *
 
-print('Welcome to the game! First player, what is your name?')
-p1 = Player(1, input())
-print('And his friend, what is your name?')
-p2 = Player(2, input())
-print('Ok... let me write it down... yep.\nNow, choose a maximum fo points.')
-game_mode = Game(int(input()), p1, p2)
-print('Great, we can start!')
-print('------------------------------')
-print('Rule: to roll a dices, print \'r\'')
+game_mode = Game()
+printer = Printer(game_mode)
+settings = printer.get_settings()
+player = settings[0]
+high_bar = settings[1]
+game_mode.set_settings(player, high_bar, printer)
 
 while game_mode.game_flag:
-    print('------------------------------')
-    print(game_mode.curr_player.name, ', your Turn!')
-    inp = input()
-    while inp != 'r':
-        inp = input()
-    else:
-        game_mode.turn()
+    turn_result = game_mode.turn()
+    if turn_result is False or len(game_mode.dices) == 0:
+        game_mode.add_dices()
