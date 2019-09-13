a6c12ac101d09a044d8140e844545dfc290ccd08
Muchacos <muchacos@ya.ru>
2019-09-13 17:35:13 +0300

    created basic cheats for dices

--- tools.py
+++ tools.py
@@ -1,6 +1,7 @@
 """Функции этого модуля упрощают работу над игровой механикой."""
 import random as r
 
+import data
 
 
 def chance(chance):
@@ -42,3 +43,27 @@ def check_combos_any(dices):
     """Возвращает True, если среди костей есть хотя бы одна комбинация."""
     return any([check_combos_row(dices), check_combos_single(dices)])
 
+
+def cheat_good_dices(amount_of_dices, *, clear=None):
+    if clear is None:
+        if chance(r.randint(10, 50)):
+            clear = True
+        else:
+            clear = False
+
+    if clear is True or amount_of_dices == 1:
+        possible_dices = data.DICES_CLEAR_COMBOS[amount_of_dices]
+        return r.choice(possible_dices)
+    else:
+        while True:
+            result = [r.randint(1, 6) for i in range(amount_of_dices)]
+            if (check_combos_any(result) is True and
+               result not in data.DICES_CLEAR_COMBOS[amount_of_dices]):
+                return result
+
+
+def cheat_bad_dices(amount_of_dices):
+    while True:
+        result = [r.choice([2, 3, 4, 6]) for i in range(amount_of_dices)]
+        if check_combos_any(result) is False:
+            return result
